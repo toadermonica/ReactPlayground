9818828b1f46d3232a0159d00fd81ab7e42c2fcc
CORP\mtoader <monica.toader@capgemini.com>
2021-10-05 14:16:47 +0200

    initial page commit

---
+++ pages/home.js
@@ -0,0 +1,14 @@
+import React from "react";
+import FormComponent from "../components/FormComponent";
+
+// home component -- landing page for users
+function HomePage(){
+    return(
+    <div>
+        <h2>Welcome Home!</h2>
+        <FormComponent/>
+    </div>
+    );
+}
+
+export default HomePage;
---
+++ pages/index.js
@@ -0,0 +1,7 @@
+import React from "react";
+import { BrowserRouter as Router } from "react-router-dom";
+import HomePage from './home';
+
+export default function Index(){
+    return <HomePage/>;
+}
